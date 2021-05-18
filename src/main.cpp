e517790f1508e5b32295b24a4323e440304424ab
Xekso <timmaxgibson@gmail.com>
2021-05-18 19:50:42 +1000

    fiddling

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
-#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
 #include <FastLED.h>
 #include <OneButton.h>
 
-#define NUM_LEDS 40
+#define NUM_LEDS 100
 #define LED_PIN 7
 #define BTN_PIN 3
+#define BOARD_LED 13  // NODEMCU board LED
+#define ESP_LED 2     // ESP8266 LED
+#define MAX_DRAW 900  // Max power draw in mW
 
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];  // Global LED array
 uint8_t patternCounter = 0;
 bool isRunning = false;
 
@@ -25,35 +27,45 @@ void nextPattern() {
 void runComet() {
   isRunning = true;
   Comet comet = Comet();
+  bool addComet;
   while (isRunning) {
-    comet.runPattern();
-    yield(); // Reset watchdog timer https://forum.arduino.cc/t/soft-wdt-reset-nodemcu/425567
+    if (random(100) > 97) {
+      addComet = true;
+    } else {
+      addComet = false;
+    }
+    comet.runPattern(addComet);
+    // yield();  // Reset watchdog(WDT) timer https://forum.arduino.cc/t/soft-wdt-reset-nodemcu/425567
   }
 }
 
 // Set up arduino
 void setup() {
+  // Power indicator
+  pinMode(BOARD_LED, OUTPUT);
+  analogWrite(BOARD_LED, HIGH);
+
+  // Set up FastLED
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(32);
-  FastLED.setMaxPowerInMilliWatts(450);
-  set_max_power_indicator_LED(2);
+  FastLED.setBrightness(64);
+  FastLED.setMaxPowerInMilliWatts(MAX_DRAW);
+  set_max_power_indicator_LED(ESP_LED);
 
   Serial.begin(57600);
 
+  // Add button listener (kinda)
   btn.attachClick(nextPattern);
 }
 
 // Main loop function
 void loop() {
+  // Select pattern
   switch (patternCounter) {
     case 0:
       runComet();
       break;
-      // case 1:
-      //   runRainbowBeat();
-      //   break;
-      // case 2:
-      //   runRedWhiteBlue();
-      //   break;
+    default:
+      runComet();
+      break;
   }
 }
